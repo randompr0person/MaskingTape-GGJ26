95aeebab435214de23bea23a9a07e6348b697327
Samjr900 <Samuel3.Reed@live.uwe.ac.uk>
2026-02-01 12:58:06 +0000

    Scissor now boots you to the menu

---
+++ Scenes/kill_zone.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	pass # Replace with function body.
