32f7a3973d97b2215a40a39f7e1885b19e7e1360
Samjr900 <Samuel3.Reed@live.uwe.ac.uk>
2026-02-01 13:32:32 +0000

    Got a win condition made

--- Scenes/kill_zone.gd
+++ Scenes/kill_zone.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+		get_tree().change_scene_to_file("res://Scenes/masking_win.tscn")
 	pass # Replace with function body.
